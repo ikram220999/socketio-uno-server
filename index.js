06a059a957e9cd1c6c8e1b20aa4e7e392cd9da66
Muhammad Ikram <lalatputih22@gmail.com>
2022-10-08 19:54:57 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+let arr = [];
+
+io.on("connection", (socket) => {
+  // console.log(`User Connected ${socket.id}`);
+  console.log("socket info", socket.id);
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", data);
+  });
+
+  socket.on("join_room", (room) => {
+    if (io.sockets.adapter.rooms.has(room)) {
+      if (io.sockets.adapter.rooms.get(room).size <= 2 ) {
+        console.log("dh ada", io.sockets.adapter.rooms);
+        socket.join(room);
+        socket.to(room).emit("hosted", 1);
+        socket.to(room).emit("user_join", socket.id);
+      }
+      //     const sockets = Array.from(io.sockets.sockets).map((socket) => socket[0]);
+      // socket.to(room).emit("list_user", sockets );
+    } else {
+      console.log("belum ada", io.sockets.adapter.rooms);
+      socket.join(room);
+      socket.to(room).emit("hosted", 0);
+      socket.to(room).emit("user_join", socket.id);
+      //     const sockets = Array.from(io.sockets.sockets).map((socket) => socket[0]);
+      // socket.to(room).emit("list_user", sockets );
+    }
+
+    const sockets = Array.from(io.sockets.sockets).map((socket) => socket[0]);
+    socket.to(room).emit("list_user", sockets);
+    console.log(sockets);
+  });
+
+  socket.on("test", (data) => {
+    console.log("test", data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("SERVER OK");
+});
