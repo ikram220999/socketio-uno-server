f332eb284a61ecdef5f4212d7c4e01bd56bb41e5
Muhammad Ikram <lalatputih22@gmail.com>
2022-11-11 00:23:17 +0800

    deployment 3

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: `https://famous-piroshki-468723.netlify.app`,
+    origin: `*`,
     
     
     // https://stately-torrone-f0cacf.netlify.app
